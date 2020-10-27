d5655c1843516ed8b9b5926eab551344a8252cdb
sanjau8 <41587586+sanjau8@users.noreply.github.com>
2020-10-27 15:07:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//     host: 'remotemysql.com:3306',
+//     user: "OBvpHD7CSf",
+//     password: "vlYNAQGrdf",
+//     database:"OBvpHD7CSf"
+//   });
+  
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//   });
+
+console.log("in")
+ var express = require('express')
+ var app = express()
+
+ app.get('/car',function(req,resp){
+     temp={'action':'L'}
+     resp.end(JSON.stringify(temp))
+ })
+   //app.listen(process.env.PORT || 3000)
+   app.listen(3000)
+
+
